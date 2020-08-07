50f27a615cac6376244aae293afe19b782804564
muhammed3ly <40716171+muhammed3ly@users.noreply.github.com>
2020-08-07 21:12:14 +0200

    dijkestra with c++

--- algo/algo/algo.cpp
+++ algo/algo/algo.cpp
@@ -16,16 +16,50 @@ int main()
 	for (int i = 0; i < edges; i++)
 	{
 		int u, v, cost;
-		cin >> u >> v;
+		cin >> u >> v >> cost;
 		graph[u].push_back({ v, cost });
 		graph[v].push_back({ u, cost });
 	}
 
-	
+	priority_queue<pair<long long, int>>pq;
+	long long maxLength[1005];
+	for (int i = 1; i <= nodes; i++)
+		maxLength[i] = 1e18;
+	maxLength[startNode] = 0;
+	pq.push({0, startNode});
+	while (pq.empty() == false)
+	{
+		long long lengthSoFar = -pq.top().first;
+		int currentNode = pq.top().second;
+		pq.pop();
 
-	if (found)
-		cout << length[endNode];
-	else
-		cout << "No path found";
+		if (currentNode == endNode)
+		{
+			cout << lengthSoFar;
+			return 0;
+		}
+
+		if (lengthSoFar != maxLength[currentNode])
+		{
+			continue;
+		}
+
+		for (int i = 0; i < graph[currentNode].size(); i++)
+		{
+			int child = graph[currentNode][i].first;
+			long long cost = graph[currentNode][i].second;
+			long long newCostToChild = cost + lengthSoFar;
+
+			if (newCostToChild < maxLength[child])
+			{
+				maxLength[child] = newCostToChild;
+				pq.push({-newCostToChild, child});
+			}
+		}
+
+	}
+
+	cout << "No Path Exist";
+
 	return 0;
 }
