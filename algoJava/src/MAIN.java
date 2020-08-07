422c08bf8ebd9fc163550067619c7dfba9094675
muhammed3ly <40716171+muhammed3ly@users.noreply.github.com>
2020-08-07 21:36:41 +0200

    Dijkestra with Java

--- algoJava/src/MAIN.java
+++ algoJava/src/MAIN.java
@@ -1,37 +1,31 @@
 import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.PriorityQueue;
 import java.util.Scanner;
 import java.util.Vector;
 
-public class MAIN {
+class Pair<T1, T2> implements Comparable<Pair>{
+    public T1 first;
+    public T2 second;
 
-    static void constructWithAdjMatrix(int nodes, int edges)
-    {
-        Scanner scan = new Scanner(System.in);
-        int[][] graph = new int[nodes + 1][nodes + 1];
-
-        for(int i = 0; i < edges ; i++)
-        {
-            int u = scan.nextInt(), v = scan.nextInt();
-            graph[u][v] = 1;
-            graph[v][u] = 1;
-        }
-
-        for(int i = 1 ; i <= nodes ; i++)
-        {
-            for(int j = 1 ; j <= nodes ; j++)
-            {
-                System.out.printf("%d ", graph[i][j]);
-            }
-            System.out.println();
-        }
+    public Pair(T1 first, T2 second) {
+        this.first = first;
+        this.second = second;
     }
+
+    @Override
+    public int compareTo(Pair o) {
+        return Long.compare((Long) this.first, (Long) o.first);
+    }
+}
+
+public class MAIN {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int nodes = scan.nextInt(), edges = scan.nextInt();
+        int nodes = scan.nextInt(), edges = scan.nextInt(), startNode = scan.nextInt(), endNode = scan.nextInt();
 
-        Vector<Vector<Integer>> graph = new Vector<>();
+        Vector<Vector<Pair<Integer, Long>>> graph = new Vector<>();
         for(int i = 0 ; i <= nodes ; i++)
         {
             graph.add(new Vector<>());
@@ -39,16 +33,50 @@ public class MAIN {
         for(int i = 0 ; i < edges ; i++)
         {
             int u = scan.nextInt(), v = scan.nextInt();
-            graph.elementAt(u).add(v);
-            graph.elementAt(v).add(u);
+            long weight = scan.nextLong();
+            graph.elementAt(u).add(new Pair<>(v, weight));
+            graph.elementAt(v).add(new Pair<>(u, weight));
         }
 
-        for(int i = 1 ; i <= nodes ; i++)
+        PriorityQueue<Pair<Long, Integer>>pq = new PriorityQueue<>();
+        Long[] maxLength = new Long[nodes + 1];
+        Arrays.fill(maxLength, Long.MAX_VALUE);
+
+        maxLength[startNode] = 0L;
+        pq.add(new Pair<>(0L, startNode));
+
+        while(!pq.isEmpty())
         {
-            System.out.printf("Node %d children: ", i);
-            for(int j = 0 ; j < graph.elementAt(i).size() ; j++)
-                System.out.printf("%d ", graph.elementAt(i).elementAt(j));
-            System.out.println();
+            Long lengthSoFar = pq.peek().first;
+            int currentNode = pq.peek().second;
+
+            pq.remove();
+
+            if(currentNode == endNode)
+            {
+                System.out.println(lengthSoFar);
+                System.exit(0);
+            }
+
+            if(lengthSoFar != maxLength[currentNode])
+                continue;
+
+            for(int i = 0 ; i < graph.elementAt(currentNode).size() ; i++)
+            {
+                int child = graph.elementAt(currentNode).elementAt(i).first;
+                long costToChild = graph.elementAt(currentNode).elementAt(i).second;
+
+                long totalToChild = costToChild + lengthSoFar;
+
+                if(totalToChild < maxLength[child])
+                {
+                    maxLength[child] = totalToChild;
+                    pq.add(new Pair<>(totalToChild, child));
+                }
+
+            }
         }
+
+        System.out.println("No Path Exists");
     }
 }
